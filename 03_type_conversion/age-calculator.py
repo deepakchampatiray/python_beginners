bbff70882a7ac405a414fece8b055382ad20b30d
Deepak Champatiray <dechamp6@in.ibm.com>
2019-09-24 20:28:47 +0530

    type conversion

---
+++ 03_type_conversion/age-calculator.py
@@ -0,0 +1,3 @@
+birthYear = input("What is your year of birth? ")
+age = 2019 - int(birthYear)
+print("Your age is " + str(age))
---
+++ 03_type_conversion/pound-to-kilo-converter.py
@@ -0,0 +1,3 @@
+weightInPound = input("Please enter the weight in pound. ")
+weightInKilo = float(weightInPound) * 0.453592
+print("Weight in kilo is " + str(weightInKilo))

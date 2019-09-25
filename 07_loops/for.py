eec380abaad08b69a1032e90491c27c28f760eaa
Deepak Champatiray <dechamp6@in.ibm.com>
2019-09-25 16:23:15 +0530

    loops

---
+++ 07_loops/for.py
@@ -0,0 +1,8 @@
+numbers = [5, 2, 5, 2, 2]
+for n in numbers:
+    str = ''
+    i = 0
+    while i < n:
+        str += 'X'
+        i += 1
+    print(str)
---
+++ 07_loops/while.py
@@ -0,0 +1,12 @@
+i = 0
+iter = 15
+while i < iter:
+    stars = ((iter - i) * ' ') + ((2*i + 1) * '*')
+    print(stars)
+    i += 1
+
+i = 0
+while i < 3:
+    stars = ((iter-1) * ' ') + (3 * '*')
+    print(stars)
+    i += 1

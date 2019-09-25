1fe3bdc4cf68c951c125773af1219f62056f0f51
Deepak Champatiray <dechamp6@in.ibm.com>
2019-09-25 10:56:55 +0530

    arithmetic operator and conditional statement

---
+++ 05_arithmetic_operators/operators.py
@@ -0,0 +1,10 @@
+# Usual operators for addition, subtraction, multiplication and division
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+
+# Modulo, integer-division and power
+print(10 % 3)
+print(10 // 3)
+print(10 ** 3)
---
+++ 06_conditional_statements/if.py
@@ -0,0 +1,11 @@
+isHot = False
+isCold = True
+
+if isHot:
+    print("It's hot out there")
+    print("Drink plenty of water")
+elif isCold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
